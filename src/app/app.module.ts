97b21a528d477cf48314c92220f308175a82789b
Pau Llobet <paullobet99@gmail.com>
2023-01-08 11:09:53 +0100

    Added navbar components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,17 +6,35 @@ import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { HomeComponent } from './home/home.component';
 import { RouterModule, Routes } from '@angular/router';
+import { ProfileComponent } from './profile/profile.component';
+import { HousesComponent } from './houses/houses.component';
+import { SensorComponent } from './sensor/sensor.component';
+import { AnalyticsComponent } from './analytics/analytics.component';
+import { SettingsComponent } from './settings/settings.component';
+import { HelpPageComponent } from './help-page/help-page.component';
 
 const appRoutes: Routes = [
   { path: 'home', component: HomeComponent },
-  { path: '', component: LoginComponent }
+  { path: '', component: LoginComponent },
+  { path: 'profile', component: ProfileComponent},
+  { path: 'houses', component: HousesComponent},
+  { path: 'sensor', component: SensorComponent},
+  { path: 'analytics', component: AnalyticsComponent},
+  { path: 'settings', component: SettingsComponent},
+  { path: 'help', component: HelpPageComponent}
 ]
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    HomeComponent
+    HomeComponent,
+    ProfileComponent,
+    HousesComponent,
+    SensorComponent,
+    AnalyticsComponent,
+    SettingsComponent,
+    HelpPageComponent
   ],
   imports: [
     BrowserModule,
